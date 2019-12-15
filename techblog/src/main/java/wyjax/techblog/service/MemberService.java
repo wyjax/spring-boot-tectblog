ff60a692abfb8be6bddd3f044e9effccb4e50efa
wyjax <umjugnki@gmail.com>
2019-12-16 01:01:42 +0900

    service work

--- techblog/src/main/java/wyjax/techblog/service/MemberService.java
+++ techblog/src/main/java/wyjax/techblog/service/MemberService.java
@@ -5,16 +5,34 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wyjax.techblog.model.Member;
 import wyjax.techblog.repository.MemberRepository;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     public void save(Member member) {
         memberRepository.save(member);
+    }
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member members = memberRepository.findMember(member.getEmail());
+
+        if (members != null) {
+            throw new IllegalStateException("Already Regist Email");
+        }
     }
 
     @Override
