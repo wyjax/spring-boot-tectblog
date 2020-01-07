266ca2ec22068d0a0a4c4efd7dc5ce29084da0a9
wyjax <umjugnki@gmail.com>
2020-01-07 18:05:18 +0900

    글 수정 및 삭제 로직 추가

--- techblog/src/main/java/wyjax/techblog/service/ContentService.java
+++ techblog/src/main/java/wyjax/techblog/service/ContentService.java
@@ -6,23 +6,27 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wyjax.techblog.model.Content;
 import wyjax.techblog.repository.ContentRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ContentService {
     private final ContentRepository contentRepository;
 
+    @Transactional
     public Page<Content> getBoardList(Pageable pageable) {
         int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber());
         pageable = PageRequest.of(page, 10, Sort.Direction.DESC, "id");
 
-        return contentRepository.findAll(pageable);
+        return contentRepository.findByIsdelete(pageable, 'N');
     }
 
+    @Transactional
     public Content save(String title, String content, String uid) {
         Content cnt = new Content();
         cnt.setTitle(title);
@@ -30,11 +34,18 @@ public class ContentService {
         cnt.setUid(uid);
         cnt.setRegdate(LocalDateTime.now());
         cnt.setViewcount(0);
+        cnt.setIsdelete('N');
         Content newcontent = contentRepository.save(cnt);
         return newcontent;
     }
 
     public Content view(Long id) {
-        return contentRepository.getOne(id);
+        return contentRepository.findByIdAndAndIsdelete(id, 'N');
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Content content = contentRepository.getOne(id);
+        content.setIsdelete('Y');
     }
 }
