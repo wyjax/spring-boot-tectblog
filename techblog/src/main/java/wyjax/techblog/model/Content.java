3cfc7cd3651d7b434ee1a25873e803e77fa4db45
wyjax <umjugnki@gmail.com>
2020-01-07 18:03:07 +0900

    isdelete 컬럼 추가

--- techblog/src/main/java/wyjax/techblog/model/Content.java
+++ techblog/src/main/java/wyjax/techblog/model/Content.java
@@ -3,6 +3,7 @@ package wyjax.techblog.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -10,7 +11,13 @@ import java.time.LocalDateTime;
 @Entity
 @Setter
 @Getter
+@DynamicInsert
 public class Content {
+    /*
+        dafault 값을 주는 방법
+        1. DynamicInsert, DynamicUpdate
+        2. PreInsert, PreUpdate
+     */
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +32,9 @@ public class Content {
 
     @Column(nullable = false)
     private String content;
+
+    @ColumnDefault("'N'")
+    private char isdelete;
 
     @ColumnDefault("0")
     private Integer viewcount;
