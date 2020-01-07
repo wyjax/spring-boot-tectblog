4131c28e42b267d232ae0862517cf57cb8fb121e
wyjax <umjugnki@gmail.com>
2020-01-07 18:03:39 +0900

    검색 쿼리 추가

--- techblog/src/main/java/wyjax/techblog/repository/ContentRepository.java
+++ techblog/src/main/java/wyjax/techblog/repository/ContentRepository.java
@@ -1,5 +1,7 @@
 package wyjax.techblog.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wyjax.techblog.model.Content;
@@ -7,4 +9,7 @@ import wyjax.techblog.model.Content;
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
+    Page<Content> findByIsdelete(Pageable pageable, char check);
+
+    Content findByIdAndAndIsdelete(Long id, char isdelete);
 }
